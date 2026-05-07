ce1bf01823d001eebb1cfbe1aa37bc0299871870
Islam Ahmed Alwa <islamalwa77@gmail.com>
2026-05-07 06:10:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,27 +4,78 @@ const h1 = document.querySelector("#page-title");
 const input = document.querySelector("#new-task");
 const addButton = document.querySelector("#add-button");
 const taskList = document.getElementById("task-list");
-try {
+const h2_tasks = document.querySelector("#tasks_text");
+
+function updateTaskCount() {
+    if (taskList.children.length === 0) {
+        h2_tasks.textContent = "No tasks";
+        return;
+    }
+    const count = taskList.children.length; 
+    h2_tasks.textContent = `Tasks : ${count}`;
+}
 const today = new Date();
 const options = [  today.getFullYear(), today.getMonth() + 1,  today.getDate() ];
 const [year, month, day] = options;
 const now = `${year}/${month}/${day}`
-   
-addButton.addEventListener("click", function() {
-    const new_item  = document.createElement("li");
-    new_item.textContent =input.value+ " " +now ;
-    const taskList = document.querySelector("#task-list");
-    taskList.appendChild(new_item);
-    input.value = "";
 
+   try {
+    addButton.addEventListener("click", function() {
+
+    if (input.value === "") return; 
+    const new_item  = document.createElement("li");
+    const check = document.createElement("input");
+    const label = document.createElement("label");
+    check.type = "checkbox";
+    const button = document.createElement("button");
+    button.textContent = "Delete";
+    button.style.direction = "rtl";
+
+    button.addEventListener("click", function() {
+        taskList.removeChild(new_item);
+        updateTaskCount();
+    });
+    
+    check.addEventListener("change", function() {
+     
+        if (check.checked) {
+            label.style.textDecoration = "line-through";
+             label.style.color = "gray";
+
+        } else {
+            label.style.textDecoration = "none";
+            label.style.color = "black";
+    }
+      
+    });
+    
+    label.textContent = " " + input.value + " " + now;
+    taskList.appendChild(new_item);
+    new_item.appendChild(check);
+    new_item.appendChild(label);
+    new_item.appendChild(button);
+    input.value = "";
+    updateTaskCount();
     new_item.addEventListener("mouseover", function() {
     new_item.style.color = "red";
  })
     new_item.addEventListener("mouseout", function() {
     new_item.style.color = "black";
  })
+    check.addEventListener("click", function() {
+                check.checked = !check.checked;
+    })
     new_item.addEventListener("click", function() {
-        new_item.remove();
+        check.checked = !check.checked;
+        if (check.checked) {
+            label.style.textDecoration = "line-through";
+             label.style.color = "gray";
+
+        } else {
+            label.style.textDecoration = "none";
+            label.style.color = "black";
+
+        }
  })
  })
   } catch (error) {
